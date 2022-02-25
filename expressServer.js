a0012e41b8930c8fad5fa90f874c05d92c727a43
Colt Skeen <93236823+coltskeen@users.noreply.github.com>
2022-02-25 13:39:50 -0500

    Added first GET request/response

--- expressServer.js
+++ expressServer.js
@@ -1,9 +1,30 @@
-import Express from "express";
+//IMPORT EXPRESS AND FS
+const express = require("express");
+const fs = require("fs");
 
-const app = Express();
+
+const app = express();
 const PORT = 8000;
+let petsData = JSON.parse(fs.readFileSync('./pets.json', 'utf-8'));
 
-app.use(Express.json());
-app.use(Express.urlencoded({extended: true}));
+//ESTABLISH MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.get();
+
+//GET REQUEST/RESPONSE
+app.get("/pets", (req, res) => {
+    req.get("Content-Type");
+    res.set("Content-Type", "application/json").status(200).send(petsData);
+});
+
+app.get("/pets/:index", (req, res) => {
+    let index = req.params.index;
+    console.log(index);
+    req.get("Content-Type");
+    res.set("Content-Type", "application/json").status(200).send(petsData[index]);
+});
+
+app.listen(PORT, () => console.log("Listening on PORT: " + PORT));
+
+//GET	/pets	200	application/json	[{ "age": 7, "kind": "rainbow", "name": "fido" }, { "age": 5, "kind": "snake", "name": "Buttons" }]
