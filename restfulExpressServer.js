778ee2d50a81939ccc91b7ae5d9540d6c3c3d096
Colt Skeen <colton.skeen@gmail.com>
2022-03-08 16:56:08 -0500

    Added DELETE request

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -15,8 +15,9 @@ app.post('/pets', (req, res) => {
     let newPet = { age, kind, name };
 
     //if age, kind or name are undefined then return bad request error
-    if (!age || !kind || !name) {
+    if (!age || Number.isInteger(age) !== true || !kind || !name) {
         res.status(400).send("Bad Request");
+        return;
     }
     
     //read the current file and write the updates to it
@@ -69,5 +70,30 @@ app.patch('/pets/:id', (req, res) => {
     });
 
 });
+
+//DELETE REQUEST
+app.delete("/pets/:id", (req, res) => {
+    let id = req.params.id;
+
+    fs.readFile("./pets.json", "utf-8", (err, data) => {
+        let parsedData = JSON.parse(data);
+        parsedData.splice(id, 1);
+
+        fs.writeFile("./pets.json", JSON.stringify(parsedData), (err) => {
+            if (err) {
+                res.status(500).send();
+            } else if (parsedData) {
+                res.set("Content-Type", "application/json").status(200).send("DELETE REQUEST SUCCESSFUL");
+            } else {
+                res.set("Content-Type", "text/plain"). status(404).send("Not Found");
+            }
+        })
+    })
+})
+
+//CATCH-ALL ERROR HANDLING
+app.use((req, res, next) => {
+    res.status(404).send("Not Found");
+})
 
 app.listen(PORT, () => console.log("Listening on PORT: " + PORT));
